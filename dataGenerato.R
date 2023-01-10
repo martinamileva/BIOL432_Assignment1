ac7cef208fcfb19b5a8073b3b26bd0fc1be36dcf
Martina <martinamileva5@gmail.com>
2023-01-10 13:34:41 -0500

    Added script to generate limb measurement data

---
+++ dataGenerato.R
@@ -0,0 +1,24 @@
+# 100 species randomly selected from vector of 5 names
+
+Species <- sample(c("Eptesicus fuscus", "Lasiurus cinereus",
+             "Lasiurus borealis", "Lasionycteris noctivagans",
+             "Myotis lucifugus"), 100, replace=T)
+
+# 100 values for limb width and length randomly selected 
+# from a distribution
+
+Limb_width <- rnorm(100,4.0,0.4)
+
+Limb_length <- rnorm(100,7.5,2.1)
+
+# 100 observers randomly selected from vector of 5 names
+
+Observer <- sample(c("Martina M", "Betty M", "Lilly M"),
+                   100, replace=T)
+
+# All vectors combined into a data frame object and exported
+
+Bat_Data <- data.frame(Species, Limb_width, Limb_length, 
+                       Observer)
+
+write.csv(Bat_Data, "C:/Users/marti/Desktop/BIOL432_Assignment1\\Bat_Data.csv")
